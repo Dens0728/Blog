ca205d35e845682522d5fe84ee4e0c76f0f71be3
Denshu <e23010@std.it-college.ac.jp>
2023-07-20 21:42:17 +0900

    [modify index.js] p34&p39 done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 const Home = () => {
 return (
+    <>
+    <header>HEADER</header>
+    <main>
+      <div>
     <h1>CUBE</h1>
-)
+    <p>アウトプットしていくサイト</p>
+    </main>
+    <footer>FOOTER</footer>
+    </>
+    )
 }
 export default Home 

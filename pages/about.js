1f657277488a0d83c8c53a6a2498f0e9ca1bbd0f
Denshu <e23010@std.it-college.ac.jp>
2023-08-16 21:48:27 +0900

    4.5 Home, About, Blogのwidth調整

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
 import Hero from 'components/hero'
+import Container from 'components/container'
+
 const about = () => {
-  return <Hero title='About' subtitle='About developement activities' />
+  return (
+    <Container>
+      <Hero title='About' subtitle='About developement activities' />
+    </Container>
+  )
 }
 export default about

90b3228313c198257a98124e8d3ad739408d5259
Denshu <e23010@std.it-college.ac.jp>
2023-08-16 21:38:28 +0900

    4.5 header,footerのwidth調整

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,15 @@
 import Logo from 'components/logo'
 import styles from 'styles/footer.module.css'
+import Container from 'components/container'
 
 const Footer = () => (
   <footer className={styles.wrapper}>
-    <div className={styles.flexContainer}>
-      <Logo />
-    </div>
+    <Container>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+    </Container>
   </footer>
 )
 

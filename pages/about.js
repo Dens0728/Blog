f53bc7b3320235fdd2db343e68485ad328f7fd94
Denshu <e23010@std.it-college.ac.jp>
2023-09-01 20:53:46 +0900

    6.3 finished

--- pages/about.js
+++ pages/about.js
@@ -9,14 +9,12 @@ import {
 } from 'components/two-column'
 import Image from 'next/legacy/image'
 import eyecatch from 'images/about.jpg'
-import Head from 'next/head'
+import Meta from 'components/meta'
 
 const about = () => {
   return (
     <Container>
-      <Head>
-        <title>アバウト</title>
-      </Head>
+      <Meta pageTitle='アバウト' />
       <Hero title='About' subtitle='About developement activities' />
       <figure>
         <Image

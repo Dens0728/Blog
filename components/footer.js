4efb954d22be7fc31753ddf3143ebad06c1f630d
Denshu <e23010@std.it-college.ac.jp>
2023-09-01 20:30:43 +0900

    5.9 finished

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,14 @@
 import Logo from 'components/logo'
 import styles from 'styles/footer.module.css'
 import Container from 'components/container'
+import Social from 'components/social'
 
 const Footer = () => (
   <footer className={styles.wrapper}>
     <Container>
       <div className={styles.flexContainer}>
         <Logo />
-        [ソーシャル]
+        <Social />
       </div>
     </Container>
   </footer>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import Layout from 'components/layout'
 import 'styles/globals.css'
 // import 'styles/sample.css'
+
+// font awesome の設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const App = ({ Component, pageProps }) => {
   return (
     <Layout>

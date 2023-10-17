a282bf72377860b76d2c932bcf94d24be1bf0db2
Denshu <e23010@std.it-college.ac.jp>
2023-10-17 21:26:06 +0900

    chapter7 ok

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
-const Home = () => {
-  return (
-    <Container>
-      <Meta />
-      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
-    </Container>
-  )
-}
-
+const Home = props => (
+  <Container>
+    <Hero {...props} />
+  </Container>
+)
+const getStaticProps = async context => ({
+  props: {
+    title: 'CUBE',
+    subtitle: 'アウトプットしていくサイト',
+    imageOn: true
+  }
+})
+export { getStaticProps }
 export default Home

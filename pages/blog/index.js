67a99dc67124d0c1525da1d973a5b9e9d4c6376c
Denshu <e23010@std.it-college.ac.jp>
2023-11-20 19:04:07 +0900

    9.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,14 +1,27 @@
+import { getAllPosts } from 'lib/api'
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import Posts from 'components/posts'
+import Post from './[slug]'
 
-const blog = () => {
+const blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+      <Posts posts={posts} />
     </Container>
   )
+}
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
 
 export default blog
